79ff62817ef81045f9fb2611bcca8053807292a4
MB-44 <menathlakvindub@gmail.com>
2025-08-22 18:27:26 +0530

    added the footer

---
+++ src/layout/index.ts
@@ -0,0 +1,4 @@
+export { default as Landing } from './landing';
+export { default as Description } from './description';
+export { default as HomeIntro } from './homeIntro';
+export { default as Footer } from './footer';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import Head from "next/head";
 import { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import PreLoader from "@/components/preloader";
-import Landing from "@/layout/landing";
-import Description from "@/layout/description";
-import Header from "@/components/header"
-import { CursorFlow } from "@/components";
+import { Landing, Description, HomeIntro, Footer } from "@/layout";
+import Header from "@/components/header";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -34,9 +32,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logo.png" />
       </Head>
-      
-      <CursorFlow hideNativeCursor />
-      
+
       <Header/>
       <main>
         <AnimatePresence mode="wait">
@@ -44,6 +40,7 @@ export default function Home() {
         </AnimatePresence>
         <Landing/>
         <Description/>
+        <Footer/>
       </main>
     </>
   );

a9d2267564c6ade7929ad5c3e79f1fe178a8f35b
MB-44 <menathlakvindub@gmail.com>
2025-10-26 19:33:13 +0530

    Redundant/Unused Code Files

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,4 +2,3 @@ export { default as AudioControlButton } from './audioButton';
 export { default as Magnetic } from './magnetic';
 export { default as Rounded } from './rounded';
 export { default as HackerText } from './hacker';
-export { default as FlowCursor } from "./cursorFlow";
